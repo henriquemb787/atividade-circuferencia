bcf90b80b78f603522049a519ff52461620e289f
henriquemb787 <henriquemb787@gmail.com>
2026-04-16 10:29:00 -0300

    Add files via upload

---
+++ circuferencia.py
@@ -0,0 +1,10 @@
+import math
+
+# Recebe o valor do raio
+raio = float(input("Digite o valor do raio: "))
+
+# Calcula o comprimento da circunferência
+comprimento = 2 * math.pi * raio
+
+# Exibe o resultado
+print(f"O comprimento da circunferência é: {comprimento:.2f}")
